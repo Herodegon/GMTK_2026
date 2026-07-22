1c3385f5430da9be5c5919bf32d3da6025d47a7c
Evan <55466688+youssefevan@users.noreply.github.com>
2026-07-22 21:56:53 +0000

    Camera and fishing rod tests

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,17 +1,28 @@
 extends Node3D
 
-var target_transform : Transform3D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var target_transform: Transform3D
+var camera_lock: bool = false
 
 func _physics_process(delta):
 	
+	if Input.is_action_just_pressed("interact"):
+		$Rod/Animator.play("Cast")
+	
+	if !camera_lock:
+		handle_camera()
+		$Camera.transform = lerp($Camera.transform, target_transform, 3.0 * delta)
+	else:
+		$Camera.transform = lerp($Camera.transform, $Up.transform, 3.0 * delta)
+
+func handle_camera():
 	if Input.is_action_just_pressed("up"):
-		target_transform = $Position1.transform
+		target_transform = $Up.transform
 	
 	if Input.is_action_just_pressed("down"):
-		target_transform = $Position2.transform
+		target_transform = $Down.transform
 	
-	$Camera.transform = lerp($Camera.transform, target_transform, 3.0 * delta)
+	if Input.is_action_just_pressed("left"):
+		target_transform = $Left.transform
+	
+	if Input.is_action_just_pressed("right"):
+		target_transform = $Right.transform
