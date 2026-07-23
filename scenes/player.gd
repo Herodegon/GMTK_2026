74f8030fe9ba69ac21a7dff49f485d8cc43e1dc1
Evan <55466688+youssefevan@users.noreply.github.com>
2026-07-22 21:30:19 -0400

    refactor: camera and input

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,23 +1,42 @@
 extends Node3D
 
+@onready var camera = %Camera
+
+@export var camera_speed: float = 3.0
+
 var target_transform: Transform3D
 var camera_lock: bool = false
 
 var interaction: int
-enum interactions {FISH, RADIO, BEER, BAIT} 
+enum interactions {FISH, RADIO, BEER, BAIT}
 
 func _physics_process(delta):
 	if !camera_lock:
-		handle_camera()
-		$Camera.transform = lerp($Camera.transform, target_transform, 3.0 * delta)
+		camera.transform = lerp(camera.transform, target_transform, camera_speed * delta)
 	else:
-		$Camera.transform = lerp($Camera.transform, $Up.transform, 3.0 * delta)
-	
+		camera.transform = lerp(camera.transform, $Up.transform, camera_speed * delta)
 
 func _input(event):
 	if event is InputEvent:
 		if event.is_pressed() and event.is_action_pressed("interact"):
 			interact()
+		
+		if !camera_lock:
+			if event.is_pressed() and event.is_action_pressed("up"):
+				target_transform = $Up.transform
+				interaction = interactions.FISH
+			
+			if event.is_pressed() and event.is_action_pressed("down"):
+				target_transform = $Down.transform
+				interaction = interactions.BAIT
+			
+			if Input.is_action_just_pressed("left"):
+				target_transform = $Left.transform
+				interaction = interactions.RADIO
+			
+			if Input.is_action_just_pressed("right"):
+				target_transform = $Right.transform
+				interaction = interactions.BEER
 
 func interact():
 	match interaction:
@@ -30,20 +49,3 @@ func interact():
 			print("beer")
 		3:
 			print("bait")
-
-func handle_camera():
-	if Input.is_action_just_pressed("up"):
-		target_transform = $Up.transform
-		interaction = interactions.FISH
-	
-	if Input.is_action_just_pressed("down"):
-		target_transform = $Down.transform
-		interaction = interactions.BAIT
-	
-	if Input.is_action_just_pressed("left"):
-		target_transform = $Left.transform
-		interaction = interactions.RADIO
-	
-	if Input.is_action_just_pressed("right"):
-		target_transform = $Right.transform
-		interaction = interactions.BEER
