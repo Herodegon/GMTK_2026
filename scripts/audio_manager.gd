801ad1522264fd9bbc0ea7adb6361d193cc95d02
Evan <55466688+youssefevan@users.noreply.github.com>
2026-07-24 18:14:21 -0400

    added radio interaction functionality

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var radio_player = $RadioPlayer
 @onready var ambience_players = $AmbiencePlayers
 
+var current_station: int = 0
 var current_ambience: AudioStreamPlayer
 
 var time: float = 0.0
@@ -11,35 +12,34 @@ func _ready():
 	time = 0.0
 	
 	radio_player.set_bus("Radio")
-	radio_player.stream = radio_stations["news"]
-	#radio_player.play()
+	radio_player.stream = radio_stations[0]
+	radio_player.play()
+	
+	EventBus.radio_interact.connect(sync_radio)
 	
 	for i in ambience_players.get_children():
 		if i is AudioStreamPlayer:
 			i.set_bus("Ambience")
 	
-	current_ambience = ambience_players.get_child(0)
-	current_ambience.play()
-	
-	await get_tree().create_timer(0.0).timeout
-	crossfade(ambience_players.get_child(1))
-	
 	# connect radio interaction to sync_radio radio via event bus
 
-var radio_stations = {
-	"music": preload("res://assets/audio/Nature Boy.ogg"),
-	"news": preload("res://assets/audio/SpongeBob SquarePants Production Music - South Pacific Island II.mp3")
-}
+var radio_stations = [
+	preload("res://assets/audio/Nature Boy.ogg"),
+	preload("res://assets/audio/sponge.mp3"),
+	preload("res://assets/audio/mario.mp3"),
+]
 
-func sync_radio(station: String):
+func sync_radio():
+	current_station += 1
+	
 	var pos = radio_player.get_playback_position()
 	var new_pos = time + radio_player.get_playback_position()
 	
 	### Test radio_player.seek(new_pos) if there are problems
 	### .seek() moves the cursor w/out resuming/playing
 	
-	radio_player.stream.stop()
-	radio_player.stream = radio_stations[station]
+	radio_player.stop()
+	radio_player.stream = radio_stations[current_station % len(radio_stations)]
 	radio_player.play(new_pos)
 
 func crossfade(next_ambience: AudioStreamPlayer):
