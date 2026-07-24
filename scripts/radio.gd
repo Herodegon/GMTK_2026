5bf1fa8d58a59f6f7462b90aa6e70c77f0f02173
8by4 <aidanquinn147@gmail.com>
2026-07-24 17:09:46 -0400

    knob spinning animation

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 var freq: int = 0
 var num_frequencies: int = 3
+var tween: Tween
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,8 +18,19 @@ func change_freq():
 	
 	match freq % num_frequencies:
 		0:
-			print("readio: news")
+			print("radio: news")
 		1:
 			print("radio: music")
 		2:
 			print("radio: talkshow")
+
+	knob_lerp(30.0)
+func knob_lerp(degrees_rotating: float) -> void:
+	if tween and tween.is_running():
+		tween.kill()
+
+	var local_axis: Vector3 = Vector3.FORWARD
+	var target_basis: Basis = $knob.transform.basis.rotated(local_axis, deg_to_rad(degrees_rotating))
+	
+	tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+	tween.tween_property($knob, "transform:basis", target_basis, 0.25)
