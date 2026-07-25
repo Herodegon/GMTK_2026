c56ddd151f8c982ce6e0b34ee07aa692c4d1ad94
8by4 <aidanquinn147@gmail.com>
2026-07-25 19:48:24 -0400

    fade out animation

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 func _on_start_pressed() -> void:
+	$AnimationPlayer.play("fade out")
+	
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_quit_pressed() -> void:
