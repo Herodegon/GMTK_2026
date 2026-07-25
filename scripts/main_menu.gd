19122466a770c5a441bee63220ed8a00292e24d6
8by4 <aidanquinn147@gmail.com>
2026-07-25 19:52:03 -0400

    fade out finished and working

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+
 func _on_start_pressed() -> void:
-	$AnimationPlayer.play("fade out")
 	
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	$AnimationPlayer.play("fade out")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
