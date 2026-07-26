7e3cdf6088e2c98652d723424c251c6dc49941bc
8by4 <aidanquinn147@gmail.com>
2026-07-26 00:56:34 -0400

    Update main_menu.gd

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,4 +9,4 @@ func _on_quit_pressed() -> void:
 	get_tree().quit()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
